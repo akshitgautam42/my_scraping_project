860b49ddac7472ca1bc65882b12f1c7f07bb9c21
Akshit <akshitgautam42@gmail.com>
2024-07-15 14:33:49 +0530

    Test modification done

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,7 +6,7 @@ from src.storage.json_storage import JsonStorage
 def test_scrape_and_save():
     settings = ScrapeSettings(pages=1)
     scraper = ScraperService("https://dentalstall.com/shop/")
-    products = scraper.scrape_products(settings)  # No 'await' needed here
+    products = scraper.scrape_products(settings)  # Removed 'await' because scrape_products is not async
     storage = JsonStorage("test_products.json")
     storage.save(products)
     assert len(products) > 0, "No products were scraped or saved"
